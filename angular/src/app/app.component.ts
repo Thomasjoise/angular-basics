09ce6cb7b98da491098645785a4f8669fd89bd40
THOMAS JOISE <joisethomas321@gmail.com>
2023-06-18 18:06:10 +0530

    init

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular';
+  message = 'this is my 3rd time creating same thingss'
+}
